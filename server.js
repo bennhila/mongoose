53a61ea591a5d9f07ca1bf7a2f6c1fc6b3ab4202
bennhila <bennhilasaif0@gmail.com>
2023-07-03 02:21:14 +0100

    commit 3

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express')
-const userRoutes=require('./routers/userRouter')
 const personRouter = require('./routers/personRouter')
 const app = express()
 const port = 5000
 app.use(express.json())
 const connectDB = require('./helpers/connectDB')
-
 connectDB()
-
 app.use('/person',personRouter)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
